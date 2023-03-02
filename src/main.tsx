e07fddc24e3396abf0caf95b4739e29582039de6
ruwpy <ruwpy.who@gmail.com>
2023-03-02 16:39:18 +0700

    remove initial code, set up router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route index />
+        <Route path="projects" />
+        <Route path="about" />
+        <Route path="project/:projectName" />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
