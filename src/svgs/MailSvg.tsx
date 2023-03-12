e416ecde65e46807b5ea6caa1ed90cb88e24ca6b
ruwpy <ruwpy.who@gmail.com>
2023-03-12 22:59:44 +0700

    add / update svgs

--- src/svgs/MailSvg.tsx
+++ src/svgs/MailSvg.tsx
@@ -10,7 +10,7 @@ export default function MailSvg() {
       <path
         d="M4.70581 12.3529V27.7941H35.294V12.3529M4.70581 12.3529H35.294M4.70581 12.3529L20.4411 19.1176L35.294 12.3529"
         stroke="#1F1F1F"
-        stroke-width="2"
+        strokeWidth="2"
       />
     </svg>
   );
