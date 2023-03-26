750542947c33578905fde429685989a22fd2cd30
ruwpy <ruwpy.who@gmail.com>
2023-03-26 15:07:11 +0700

    refactor button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,60 +1,37 @@
 import { ReactNode } from "react";
 import "./Button.scss";
-import { motion, Variants } from "framer-motion";
-import ArrowSvg from "../../svgs/ArrowSvg";
+import { motion as m, Variants } from "framer-motion";
+import { ArrowSvg } from "../Svgs";
 
 interface ButtonProps {
   className?: string;
   children?: ReactNode;
   type?: "text" | "icon";
-  direction?: "left" | "right" | "bottom";
-  style: "primary" | "secondary" | "ternary";
-  isArrow?: boolean;
+  buttonStyle: "primarywdot" | "secondarywdot";
+  fullWidth?: boolean;
   size?: "big" | "small";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function Button({
-  className,
   type,
   children,
   size,
-  isArrow,
-  direction,
-  style,
-  ...props
+  fullWidth,
+  buttonStyle,
+  onClick,
 }: ButtonProps) {
-  const buttonVariants: Variants = {
-    onHover: {
-      translateX: "-4px",
-      translateY: "-4px",
-      boxShadow:
-        style === "primary"
-          ? "6px 6px 0 0 var(--indigo)"
-          : "6px 6px 0 0 var(--indigo)",
-    },
-    initial: {
-      translateX: "0px",
-      translateY: "0px",
-      boxShadow:
-        style === "primary" ? "0 0 0 0 var(--indigo)" : "0 0 0 0 var(--indigo)",
-    },
-  };
+  const buttonVariants: Variants = {};
 
   return (
-    <motion.button
+    <m.button
       transition={{ type: "spring", duration: 0.15 }}
       variants={buttonVariants}
-      whileHover={"onHover"}
-      initial={"initial"}
-      whileTap={"initial"}
-      className={`button ${type} ${type !== "icon" && "text"} ${size} ${style}`}
-      {...props}
+      onClick={onClick}
+      className={`button ${type} ${size} ${buttonStyle} ${fullWidth && "w100"}`}
     >
-      {type === "icon" && isArrow ? (
-        <ArrowSvg direction={direction} iconStyle={style} />
-      ) : (
-        children
-      )}
-    </motion.button>
+      {children}
+      <ArrowSvg />
+    </m.button>
   );
 }
