61590159a473b0cf1a97370492615f74d6445c26
ruwpy <ruwpy.who@gmail.com>
2023-03-26 15:09:04 +0700

    add svgs

---
+++ src/components/Svgs/barcodes/index.tsx
@@ -0,0 +1,287 @@
+export function GifttoBar() {
+  return (
+    <svg
+      width="90"
+      height="40"
+      viewBox="0 0 119 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.13536 0.833374H0.811035V49.1667H3.13536V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M5.45953 0.833374H4.29736V49.1667H5.45953V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M8.94634 0.833374H7.78418V49.1667H8.94634V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M14.7569 0.833374H13.5947V49.1667H14.7569V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M19.4059 0.833374H17.0815V49.1667H19.4059V0.833374Z"
+        fill="black"
+      />
+      <path d="M21.73 0.833374H20.5679V49.1667H21.73V0.833374Z" fill="black" />
+      <path
+        d="M27.5406 0.833374H26.3784V49.1667H27.5406V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M34.5138 0.833374H32.1895V49.1667H34.5138V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M36.8379 0.833374H35.6758V49.1667H36.8379V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M40.3248 0.833374H39.1626V49.1667H40.3248V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M43.8111 0.833374H41.4868V49.1667H43.8111V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M49.6216 0.833374H48.4595V49.1667H49.6216V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M53.1085 0.833374H51.9463V49.1667H53.1085V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M60.0813 0.833374H55.4326V49.1667H60.0813V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M62.4058 0.833374H61.2437V49.1667H62.4058V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M65.8921 0.833374H64.73V49.1667H65.8921V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M72.865 0.833374H68.2163V49.1667H72.865V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M75.1895 0.833374H74.0273V49.1667H75.1895V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M78.6758 0.833374H77.5137V49.1667H78.6758V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M86.8112 0.833374H82.1626V49.1667H86.8112V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M89.1353 0.833374H87.9731V49.1667H89.1353V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M92.6217 0.833374H90.2974V49.1667H92.6217V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M96.1085 0.833374H94.9463V49.1667H96.1085V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M100.757 0.833374H99.5947V49.1667H100.757V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M105.406 0.833374H103.082V49.1667H105.406V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M112.379 0.833374H108.892V49.1667H112.379V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M114.703 0.833374H113.541V49.1667H114.703V0.833374Z"
+        fill="black"
+      />
+      <path
+        d="M118.19 0.833374H115.865V49.1667H118.19V0.833374Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
+export function LettreBar() {
+  return (
+    <svg
+      width="85"
+      height="45"
+      viewBox="0 0 202 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M4 0H0V100H4V0Z" fill="black" />
+      <path d="M8 0H6V100H8V0Z" fill="black" />
+      <path d="M14 0H12V100H14V0Z" fill="black" />
+      <path d="M24 0H22V100H24V0Z" fill="black" />
+      <path d="M34 0H30V100H34V0Z" fill="black" />
+      <path d="M42 0H36V100H42V0Z" fill="black" />
+      <path d="M46 0H44V100H46V0Z" fill="black" />
+      <path d="M56 0H52V100H56V0Z" fill="black" />
+      <path d="M60 0H58V100H60V0Z" fill="black" />
+      <path d="M70 0H66V100H70V0Z" fill="black" />
+      <path d="M78 0H72V100H78V0Z" fill="black" />
+      <path d="M86 0H84V100H86V0Z" fill="black" />
+      <path d="M92 0H88V100H92V0Z" fill="black" />
+      <path d="M100 0H94V100H100V0Z" fill="black" />
+      <path d="M108 0H106V100H108V0Z" fill="black" />
+      <path d="M114 0H110V100H114V0Z" fill="black" />
+      <path d="M122 0H120V100H122V0Z" fill="black" />
+      <path d="M130 0H124V100H130V0Z" fill="black" />
+      <path d="M134 0H132V100H134V0Z" fill="black" />
+      <path d="M144 0H140V100H144V0Z" fill="black" />
+      <path d="M148 0H146V100H148V0Z" fill="black" />
+      <path d="M160 0H154V100H160V0Z" fill="black" />
+      <path d="M168 0H164V100H168V0Z" fill="black" />
+      <path d="M172 0H170V100H172V0Z" fill="black" />
+      <path d="M180 0H176V100H180V0Z" fill="black" />
+      <path d="M192 0H186V100H192V0Z" fill="black" />
+      <path d="M196 0H194V100H196V0Z" fill="black" />
+      <path d="M202 0H198V100H202V0Z" fill="black" />
+    </svg>
+  );
+}
+export function RecipeBar() {
+  return (
+    <svg
+      width="85"
+      height="45"
+      viewBox="0 0 202 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M4 0H0V100H4V0Z" fill="black" />
+      <path d="M8 0H6V100H8V0Z" fill="black" />
+      <path d="M14 0H12V100H14V0Z" fill="black" />
+      <path d="M24 0H22V100H24V0Z" fill="black" />
+      <path d="M30 0H28V100H30V0Z" fill="black" />
+      <path d="M42 0H34V100H42V0Z" fill="black" />
+      <path d="M46 0H44V100H46V0Z" fill="black" />
+      <path d="M52 0H48V100H52V0Z" fill="black" />
+      <path d="M58 0H56V100H58V0Z" fill="black" />
+      <path d="M68 0H66V100H68V0Z" fill="black" />
+      <path d="M78 0H76V100H78V0Z" fill="black" />
+      <path d="M84 0H80V100H84V0Z" fill="black" />
+      <path d="M90 0H88V100H90V0Z" fill="black" />
+      <path d="M102 0H98V100H102V0Z" fill="black" />
+      <path d="M106 0H104V100H106V0Z" fill="black" />
+      <path d="M112 0H110V100H112V0Z" fill="black" />
+      <path d="M116 0H114V100H116V0Z" fill="black" />
+      <path d="M128 0H120V100H128V0Z" fill="black" />
+      <path d="M134 0H132V100H134V0Z" fill="black" />
+      <path d="M140 0H136V100H140V0Z" fill="black" />
+      <path d="M146 0H144V100H146V0Z" fill="black" />
+      <path d="M162 0H154V100H162V0Z" fill="black" />
+      <path d="M166 0H164V100H166V0Z" fill="black" />
+      <path d="M172 0H170V100H172V0Z" fill="black" />
+      <path d="M180 0H176V100H180V0Z" fill="black" />
+      <path d="M192 0H186V100H192V0Z" fill="black" />
+      <path d="M196 0H194V100H196V0Z" fill="black" />
+      <path d="M202 0H198V100H202V0Z" fill="black" />
+    </svg>
+  );
+}
+export function TypedashBar() {
+  return (
+    <svg
+      width="90"
+      height="40"
+      viewBox="0 0 246 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M4 0H0V100H4V0Z" fill="black" />
+      <path d="M8 0H6V100H8V0Z" fill="black" />
+      <path d="M14 0H12V100H14V0Z" fill="black" />
+      <path d="M26 0H22V100H26V0Z" fill="black" />
+      <path d="M34 0H28V100H34V0Z" fill="black" />
+      <path d="M42 0H40V100H42V0Z" fill="black" />
+      <path d="M50 0H44V100H50V0Z" fill="black" />
+      <path d="M56 0H52V100H56V0Z" fill="black" />
+      <path d="M60 0H58V100H60V0Z" fill="black" />
+      <path d="M72 0H66V100H72V0Z" fill="black" />
+      <path d="M80 0H74V100H80V0Z" fill="black" />
+      <path d="M86 0H82V100H86V0Z" fill="black" />
+      <path d="M90 0H88V100H90V0Z" fill="black" />
+      <path d="M100 0H96V100H100V0Z" fill="black" />
+      <path d="M104 0H102V100H104V0Z" fill="black" />
+      <path d="M112 0H110V100H112V0Z" fill="black" />
+      <path d="M118 0H114V100H118V0Z" fill="black" />
+      <path d="M126 0H124V100H126V0Z" fill="black" />
+      <path d="M134 0H132V100H134V0Z" fill="black" />
+      <path d="M138 0H136V100H138V0Z" fill="black" />
+      <path d="M148 0H144V100H148V0Z" fill="black" />
+      <path d="M158 0H154V100H158V0Z" fill="black" />
+      <path d="M166 0H160V100H166V0Z" fill="black" />
+      <path d="M170 0H168V100H170V0Z" fill="black" />
+      <path d="M180 0H176V100H180V0Z" fill="black" />
+      <path d="M188 0H186V100H188V0Z" fill="black" />
+      <path d="M192 0H190V100H192V0Z" fill="black" />
+      <path d="M200 0H198V100H200V0Z" fill="black" />
+      <path d="M208 0H204V100H208V0Z" fill="black" />
+      <path d="M218 0H216V100H218V0Z" fill="black" />
+      <path d="M224 0H220V100H224V0Z" fill="black" />
+      <path d="M236 0H230V100H236V0Z" fill="black" />
+      <path d="M240 0H238V100H240V0Z" fill="black" />
+      <path d="M246 0H242V100H246V0Z" fill="black" />
+    </svg>
+  );
+}
+export function WthrBar() {
+  return (
+    <svg
+      width="85"
+      height="45"
+      viewBox="0 0 202 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M4 0H0V100H4V0Z" fill="black" />
+      <path d="M8 0H6V100H8V0Z" fill="black" />
+      <path d="M14 0H12V100H14V0Z" fill="black" />
+      <path d="M28 0H22V100H28V0Z" fill="black" />
+      <path d="M32 0H30V100H32V0Z" fill="black" />
+      <path d="M42 0H38V100H42V0Z" fill="black" />
+      <path d="M48 0H44V100H48V0Z" fill="black" />
+      <path d="M56 0H50V100H56V0Z" fill="black" />
+      <path d="M64 0H62V100H64V0Z" fill="black" />
+      <path d="M70 0H66V100H70V0Z" fill="black" />
+      <path d="M78 0H76V100H78V0Z" fill="black" />
+      <path d="M82 0H80V100H82V0Z" fill="black" />
+      <path d="M92 0H88V100H92V0Z" fill="black" />
+      <path d="M100 0H98V100H100V0Z" fill="black" />
+      <path d="M108 0H102V100H108V0Z" fill="black" />
+      <path d="M114 0H110V100H114V0Z" fill="black" />
+      <path d="M120 0H118V100H120V0Z" fill="black" />
+      <path d="M128 0H122V100H128V0Z" fill="black" />
+      <path d="M134 0H132V100H134V0Z" fill="black" />
+      <path d="M140 0H136V100H140V0Z" fill="black" />
+      <path d="M148 0H146V100H148V0Z" fill="black" />
+      <path d="M156 0H154V100H156V0Z" fill="black" />
+      <path d="M162 0H158V100H162V0Z" fill="black" />
+      <path d="M172 0H170V100H172V0Z" fill="black" />
+      <path d="M180 0H176V100H180V0Z" fill="black" />
+      <path d="M192 0H186V100H192V0Z" fill="black" />
+      <path d="M196 0H194V100H196V0Z" fill="black" />
+      <path d="M202 0H198V100H202V0Z" fill="black" />
+    </svg>
+  );
+}
